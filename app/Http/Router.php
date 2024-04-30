a325403b5367ea66ebf8ccf405ca5fcb5f4d1b3d
skunbydev <skunbydev@gmail.com>
2024-04-30 10:24:41 -0300

    routers created

---
+++ app/Http/Router.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http;
+
+use \Closure;
+use \Exception;
+
+class Router
+{
+  private $url = '';
+  private $prefix = '';
+
+  private $routes = [];
+
+  private $request;
+
+  public function __construct($url)
+  {
+    $this->request = new Request();
+    $this->url = $url;
+    $this->setPrefix();
+  }
+
+  private function setPrefix()
+  {
+    $parseUrl = parse_url($this->url);
+    $this->prefix = $parseUrl['path'] ?: '';
+  }
+  private function addRoute($method, $route, $params = [])
+  {
+    foreach ($params as $key => $value) {
+      if ($value instanceof Closure) {
+        $params['controller'] = $value;
+        unset($params[$key]);
+        continue;
+      }
+    }
+
+    $patternRoute = '/^' . str_replace('/', '\/', $route) . '$/';
+    $this->routes[$patternRoute][$method] = $params;
+
+
+  }
+  public function get($route, $params = [])
+  {
+    return $this->addRoute('GET', $route, $params);
+  }
+  public function POST($route, $params = [])
+  {
+    return $this->addRoute('POST', $route, $params);
+  }
+  public function PUT($route, $params = [])
+  {
+    return $this->addRoute('PUT', $route, $params);
+  }
+  public function DELETE($route, $params = [])
+  {
+    return $this->addRoute('DELETE', $route, $params);
+  }
+
+  private function getUri()
+  {
+    $uri = $this->request->getUri();
+    $xUri = strlen($this->prefix) ? explode($this->prefix, $uri) : [$uri];
+
+    return end($xUri);
+  }
+
+  private function getRoute()
+  {
+    $uri = $this->getUri();
+    $httpMethod = $this->request->getHttpMethod();
+
+    foreach ($this->routes as $patternRoute => $methods) {
+      if (preg_match($patternRoute, $uri)) {
+        if ($methods[$httpMethod]) {
+          return $methods[$httpMethod];
+        }
+        throw new Exception("Metódo não é permitido", 405);
+      }
+    }
+    throw new Exception("URL não encontrada", 404);
+  }
+
+  public function run()
+  {
+    try {
+      $route = $this->getRoute();
+      echo '<pre>';
+      print_r($route);
+      echo '</pre>';
+      exit;
+    } catch (Exception $e) {
+      return new Response($e->getCode(), $e->getMessage());
+    }
+  }
+}
+
+?>
--- index.php
+++ index.php
@@ -2,14 +2,25 @@
 require __DIR__ . '/vendor/autoload.php';
 
 
-use \App\Controller\Pages\Home;
+//use \App\Controller\Pages\Home;
+use App\Controller\Pages\Home;
+use \App\Http\Router;
+use \App\Http\Response;
+
+define('URL', 'http://localhost/ApiRestMVC');
+
+$obRouter = new Router(URL);
+
+$obRouter->get('/', [
+  function () {
+    return new Response(200, Home::getHome());
+  }
+]);
+
+$obRouter
+  ->run()
+  ->sendResponse();
 
 
-$obResponse = new \App\Http\Response(200, 'Sucesso');
-$obResponse->sendResponse();
-//echo Page::getPage();
-
-exit;
-echo Home::getHome();
 
 ?>
