df105032783c2038bf5032f9a1274b074c518c9d
skunbydev <skunbydev@gmail.com>
2024-05-06 14:56:26 -0300

    view testimony

--- app/Controller/Pages/Testimony.php
+++ app/Controller/Pages/Testimony.php
@@ -8,12 +8,35 @@ use \App\Model\Entity\Organization;
 
 class Testimony extends Page
 {
+  private static function getTestimonyItens()
+  {
+    $itens = '';
+    $results = EntityTestimony::getTestimonies(null, 'id DESC');
+    if ($results->rowCount() == 0) {
+      $itens = '<div class="card text-dark mb-3">
+      <h5 class="card-header"></h5>
+      <div class="card-body">
+        nenhum depoimento encontrado...
+      </div>
+    </div>';
+      return $itens;
+    }
+
+    while ($obTestimony = $results->fetchObject(EntityTestimony::class)) {
+      $itens .= View::render('pages/testimony/item', [
+        'nome_usuario' => $obTestimony->nome_usuario,
+        'mensagem_usuario' => $obTestimony->mensagem_usuario,
+        'data_usuario' => date('d/m/Y H:i:s', strtotime($obTestimony->data_usuario))
+      ]);
+    }
+    return $itens;
+  }
   public static function getTestimonies()
   {
     $obOrganization = new Organization;
 
     $content = View::render('pages/testimonies', [
-
+      'itens' => self::getTestimonyItens()
     ]);
     return parent::getPage('Depoimentos > ', $content);
   }
@@ -23,7 +46,7 @@ class Testimony extends Page
     $obTestimony = new EntityTestimony;
     $obTestimony->nome_usuario = $postVars['nome_usuario'];
     $obTestimony->mensagem_usuario = $postVars['mensagem_usuario'];
-    //$obTestimony->data = $postVars['data_usuario'];
+    echo "<script>alert('Depoimento enviado com sucesso')</script>";
     $obTestimony->cadastrar();
     return self::getTestimonies();
   }
--- app/Model/Entity/Testimony.php
+++ app/Model/Entity/Testimony.php
@@ -16,14 +16,18 @@ class Testimony
   public function cadastrar()
   {
     date_default_timezone_set('America/Recife');
-    $this->data_usuario = date('d-m-Y H:i:s');
+    $this->data_usuario = date('Y-m-d H:i:s'); // Formato corrigido
     $this->id = (new Database('depoimentos'))->insert([
       'nome_usuario' => $this->nome_usuario,
       'mensagem_usuario' => $this->mensagem_usuario,
       'data_usuario' => $this->data_usuario,
     ]);
 
-
+    return true;
+  }
+  public static function getTestimonies($where = null, $order = null, $limit = null, $fields = '*')
+  {
+    return (new Database('depoimentos'))->select($where, $order, $limit, $fields);
   }
 }
 
